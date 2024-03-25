0e1b88fc986df083322663d66fa955a9fb584579
Usuario <Usuario@CPD03-M14>
2024-03-25 15:57:57 -0300

    Adicionar arquivos de projeto.

---
+++ Calculador/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculador
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Calculador/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿namespace Calculador
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void zerar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maismenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subtrair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
